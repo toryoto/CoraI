4379627239c03dd8d2896d003d17e79c50bfcfec
toryoto <ryoto.taga12@gmail.com>
2025-05-11 02:24:59 +0900

    chat ui

--- src/app/assistant.tsx
+++ src/app/assistant.tsx
@@ -2,10 +2,10 @@
 
 import { AssistantRuntimeProvider } from '@assistant-ui/react'
 import { useChatRuntime } from '@assistant-ui/react-ai-sdk'
-import { ThreadList } from '@/components/assistant-ui/thread-list'
 import { Thread } from '@/components/assistant-ui/thread'
+import { ThreadList } from '@/components/assistant-ui/thread-list'
 
-export default function Chat() {
+export const Assistant = () => {
   const runtime = useChatRuntime({
     api: '/api/chat',
   })
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from '@/components/NavBar'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,8 +18,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="jp">
         <body className={inter.className}>
-          <NavBar />
-          <main className="container mx-auto py-4 px-2">{children}</main>
+          <TooltipProvider>
+            <NavBar />
+            <main className="container mx-auto py-4 px-2">{children}</main>
+          </TooltipProvider>
         </body>
       </html>
     </ClerkProvider>
